322d1def2979a41972e310443d2a72b8146e6517
pablorodrigs <pablorodrigs6@gmail.com>
2025-02-22 19:32:57 -0300

    Primeiro commit do projeto

---
+++ menu/src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Card } from './components/card/card';
+import { FoodData } from './interface/FoodData';
+import { useFoodData } from './components/hooks/userFoodData';
+import { CreateModal } from './components/create-modal/create-modal'; // Importação do CreateModal
+
+function App() {
+  const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev);
+  }
+
+  return (
+    <div className="container">
+      <h1>Cardápio</h1>
+      <div className="card-grid">
+        {data?.map(foodData => 
+          <Card
+            price={foodData.price} 
+            title={foodData.title} 
+            image={foodData.image}
+          />
+        )}
+      </div>
+      {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+      <button onClick={handleOpenModal}>Novo</button>
+    </div>
+  );
+}
+
+export default App;
